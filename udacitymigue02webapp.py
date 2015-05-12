c9d30460829cddbd6f665b275d9ca8c730c30801
migue02 <miguee02@gmail.com>
2015-05-12 20:27:33 +0200

    Nuevos cambios

--- udacitymigue02webapp.py
+++ udacitymigue02webapp.py
@@ -1,10 +1,24 @@
 import webapp2
 
+form = """
+	<form action = "/testform">
+		<input name="q">
+		<input type="submit">
+	</form>
+"""
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        q = self.request.get("q")
+        self.response.write(q)
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write(self.request)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+							   ('/testform', TestHandler)]
+							   , debug=True)
